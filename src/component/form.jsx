d8bafa15a05c94a32d988cb89efa7cbdff8686be
eric.lau@paa.net.au <eric@paa.net.au>
2025-05-14 14:54:12 +0800

    revamp file structure

---
+++ src/component/form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Form() {
+
+    return(
+        <>
+            <p>Yo this is the form</p>
+            <form>
+                <label> 
+                    <input type='text' value="t"/>
+                </label>
+            </form>
+        </>
+    )
+}
