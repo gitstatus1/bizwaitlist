cd0ae5abd6f48648e6f170f265b00c06f7ea5b25
eric.lau@paa.net.au <eric@paa.net.au>
2025-06-20 10:15:50 +0800

    added no background logo png

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './styles/App.css'
 import Form from './component/form.jsx'
+import logo from './assets/merbau_logo1_nobg.png'
 
 function App() {
 
   return (
     <div className = "main">
       <div className = "top-div">
+        <img src={logo} className = 'logo'/>
         <p className= 'title'>Merbau Coffee</p>
-        <p className= 'subtitle'>"Curated Coffee Gear, Crafted for Connoiseurs"</p>
       </div>
+        <p className= 'subtitle'>"Curated Coffee Gear, Crafted for Connoiseurs"</p>
       <Form />
     </div>
   )
